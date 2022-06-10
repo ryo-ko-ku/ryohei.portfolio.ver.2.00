559e80b87e44eb411add5c04d0b85c408d80e055
ryo-ko-ku <69999863+ryo-ko-ku@users.noreply.github.com>
2022-06-10 10:26:01 +0900

    Add files via upload

--- Script1.js
+++ Script1.js
@@ -26,5 +26,15 @@ $(function () {
         $(".history-list").hide().fadeIn("slow");
     });
 
+    $(window).scroll(function () {
+        const windowHeight = $(window).height();
+        const scroll = $(window).scrollTop();
 
+        $('.element').each(function () {
+            const targetPosition = $(this).offset().top;
+            if (scroll > targetPosition - windowHeight + 100) {
+                $(this).addClass("is-fadein");
+            }
+        });
+    });
 });
